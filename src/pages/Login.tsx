a05387382fcc79326b7a9c905aa89ed9368babc8
imadnanhassan <abulhassan.dev@gmail.com>
2025-01-15 02:24:49 +0600

    Implement login form with validation and API integration

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,28 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 export default function Login() {
-  return <div>Login</div>;
+  const { register, handleSubmit } = useForm();
+  const [login, { data, error }] = useLoginMutation();
+
+  console.log("data =>", data);
+  console.log("error =>", error);
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
 }
