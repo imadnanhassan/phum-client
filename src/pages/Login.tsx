d25a5c02e5032f0f06fed3f120a07ca5699eb788
imadnanhassan <abulhassan.dev@gmail.com>
2025-01-15 02:54:10 +0600

    Enhance login functionality with default values and user info structure; integrate base API into Redux store

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,20 +3,31 @@ import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 
 export default function Login() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0002",
+      password: "admin123",
+    },
+  });
   const [login, { data, error }] = useLoginMutation();
 
   console.log("data =>", data);
   console.log("error =>", error);
 
   const onSubmit = (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+
+    login(userInfo);
     console.log(data);
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="id">ID: </label>
-        <input type="text" id="id" {...register("id")} />
+        <input type="text" id="id" {...register("userId")} />
       </div>
       <div>
         <label htmlFor="password">Password: </label>
