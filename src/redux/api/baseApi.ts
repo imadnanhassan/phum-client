c3264a2db99544a98b32bcfc42191f5af50f5cc9
imadnanhassan <abulhassan.dev@gmail.com>
2025-01-16 20:28:16 +0600

    Update base API to use token refresh logic for improved authentication handling

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -63,6 +63,6 @@ const baseQueryWithRefreshToken: BaseQueryFn<
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
